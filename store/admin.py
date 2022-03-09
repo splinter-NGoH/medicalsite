94de1938bdb3013c3a2c52bdb38c3704ad117471
Ahmed Mansy <ahmedmansy265@gmail.com>
2022-03-09 11:50:46 +0200

    bug fixed

--- store/admin.py
+++ store/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Product, CustomerRequests, HomeReviews
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name_in_inglish', 'product_name_in_arabic', 'slug', 'price', 'is_available', 'category__category_name_inenglish', "created_date")
+    list_display = ('product_name_in_inglish', 'product_name_in_arabic', 'slug', 'price', 'is_available',  "created_date")
     prepopulated_fields = {'slug': ('product_name_in_inglish',)}
-    search_fields = ('product_name_in_inglish', 'product_name_in_arabic', 'slug', 'price', 'is_available', 'category__category_name_inenglish', "created_date")
+    search_fields = ('product_name_in_inglish', 'product_name_in_arabic', 'slug', 'price', 'is_available',  "created_date")
     filter_fields = ( 'price', 'is_available', 'category', "created_date")
 
     
