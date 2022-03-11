074d20f540e6aa653df9fac44374e1409ac733b8
Ahmed Mansy <ahmedmansy265@gmail.com>
2022-03-11 04:52:09 +0200

    before store

--- medicalrcommerce/settings.py
+++ medicalrcommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r&3sjbfzz062(8g2*+4keu6)7yb&u4dq+qi6qa@#ldmx6#icz*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
